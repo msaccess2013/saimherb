542682a89938d59bf2624412ec3a111f6174aecc
sandland <msaccess2013@gmail.com>
2016-08-10 00:04:10 +0700

    structure success, basic non twig template!!

---
+++ bootstrap/app.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+$app = new \Slim\App([
+    'setting' => [
+        'displayErrorDetails' => true,
+    ]
+]);
+
+require __DIR__ . '/../app/routes.php';
+
